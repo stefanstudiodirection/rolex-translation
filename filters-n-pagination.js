d3ecf65a78eedf110bd256fc5eb4b73002ae2be7
nradosevic <nradoshevic@gmail.com>
2023-05-30 15:39:10 +0200

    Update filters-n-pagination.js

--- filters-n-pagination.js
+++ filters-n-pagination.js
@@ -248,7 +248,7 @@ function createPaginationForProducts(itemsPerPage, reset) {
         document.getElementsByClassName('prev-page')[0].style.display = 'block';
     }
     
-    if (((paginationData.productItems.length - 1) / paginationData.itemsPerPage + 1) > parseInt(paginationData.currentPage)) {
+    if (((paginationData.productItems.length - 1) / paginationData.itemsPerPage + 1) === parseInt(paginationData.currentPage)) {
         document.getElementsByClassName('next-page')[0].style.display = 'none';
     } else {
         document.getElementsByClassName('next-page')[0].style.display = 'block';
