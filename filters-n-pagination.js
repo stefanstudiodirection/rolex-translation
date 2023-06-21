3dfda3f641b82fbe716b3e090a9b5288009c8d93
nradosevic <nradoshevic@gmail.com>
2023-06-21 12:08:36 +0200

    Update filters-n-pagination.js

--- filters-n-pagination.js
+++ filters-n-pagination.js
@@ -1,3 +1,4 @@
+
 function updateHreflang() {
   // Access the query string from the browser URL
   const queryString = window.location.search;
@@ -52,6 +53,8 @@ function fetchAndModifyProducts() {
       for (let i = 0; i < cachedProductsArray.length; i++) {
         $('#products-container').append(cachedProductsArray[i]);
       }
+      rewriteRelativeURLs();
+      generateI18nTags();
       createPaginationForProducts(18);
       return Promise.resolve();
     }
@@ -79,6 +82,8 @@ function fetchAndModifyProducts() {
               });
               localStorage.setItem('parsedProducts', JSON.stringify(parsedProductsArray));
 
+              rewriteRelativeURLs();
+              generateI18nTags();
               createPaginationForProducts(18);
               resolve();
             }
@@ -231,10 +236,13 @@ function displayItems(itemsPerPage) {
 }
 
 function updatePageNumbers(itemsPerPage) {
-    const pageContainer = document.getElementById('page-container');
-    pageContainer.textContent = 'Page ' + paginationData.currentPage;
-    paginationData.totalPages = Math.ceil(paginationData.totalItems / itemsPerPage);
+  const pageContainer = document.getElementById('page-container');
+  pageContainer.textContent = 'Page ' + paginationData.currentPage;
+  pageContainer.setAttribute('data-i18n', 'Page ' + paginationData.currentPage);
+  paginationData.totalPages = Math.ceil(paginationData.totalItems / itemsPerPage);
+  translateContent();
 }
+
 
 function handlePrevButtonClick() {
     if (paginationData.currentPage > 1) {
