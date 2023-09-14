3baec9ec8d75716b1d7a57cda159e8ce13fd6c42
Nikola Radosevic <nikola@Nikolas-MacBook-Pro-2.local>
2023-09-14 09:51:01 +0200

    adding vlada's script

--- scripts.js
+++ scripts.js
@@ -4,7 +4,20 @@
   .catch(error => console.log('Error fetching translation data:', error));
 
   
-
+const menuButton = document.querySelector('.header__menu-button.w-nav-button');
+const menuObserver = new MutationObserver((mutations) => {
+  for (const mutation of mutations) {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+      const classes = mutation.target.classList;
+      if (classes.contains('w--open')) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+      }
+    }
+  }
+});
+menuObserver.observe(menuButton, { attributes: true });
 
 
 
