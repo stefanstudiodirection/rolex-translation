90e5d5f97fd3e40b5b5204f585d3cdc1bc0ac7db
Nikola Radosevic <nikola@Nikolas-MacBook-Pro-2.local>
2023-12-29 17:17:17 +0100

    adding custom mail sending

--- scripts.js
+++ scripts.js
@@ -625,3 +625,66 @@ window.addEventListener('CookiebotOnDecline', function (e) {
   }
  }
 }, false);
+
+function submitForm() {
+    // Get language from the URL
+    const url = window.location.href;
+    const urlParts = url.split('/');
+    let lang = "rs-sr"; // Default language if not found
+
+    for (const part of urlParts) {
+        if (part.length === 5 && part.includes('-')) {
+            lang = part;
+            break;
+        }
+    }
+
+    // Get email from the input field
+    const emailTo = document.getElementById("Email-2").value;
+
+    // Get other form values
+    const title = document.getElementById("Title").value;
+    const firstName = document.getElementById("First-name").value;
+    const lastName = document.getElementById("Last-name").value;
+    const countryCode = document.getElementById("country__code").value;
+    const phoneNumber = document.getElementById("Phone-number").value;
+    const country = document.getElementById("country").value;
+    const message = document.getElementById("Message").value;
+
+    // Create JSON object
+    const formData = {
+        lang,
+        emailTo,
+        title,
+        firstName,
+        lastName,
+        countryCode,
+        phoneNumber,
+        country,
+        message,
+    };
+
+    console.log('body');
+    console.log(formData);
+
+    // Make a POST request
+    fetch("https://www.petitegeneve.com/send-mail/general-inquiry", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Form submitted successfully!");
+        } else {
+            alert("Form submission failed. Please try again later.");
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        alert("Form submission failed. Please try again later.");
+    });
+}
+
